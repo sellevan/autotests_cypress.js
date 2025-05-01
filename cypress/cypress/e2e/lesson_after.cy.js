36b1220efddb08477dfa26f0478fe2eec90e024d
sellevan <sellevan0330@gmail.com>
2025-05-01 18:51:42 +0300

    lesson_after.cy.js

--- cypress/cypress/e2e/lesson_after.cy.js
+++ cypress/cypress/e2e/lesson_after.cy.js
@@ -10,23 +10,23 @@ describe('Проверка авторизации', function () {
         });
 
    it('Верный пароль и верный логин', function () {
-        cy.get('#mail').type('german@dolnikov.ru');
-        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#mail').type('USER_LOGIN');
+        cy.get('#pass').type('USER_PASSWORD');
         cy.get('#loginButton').click();
         cy.get('#messageHeader').should('be.visible');
         cy.get('#messageHeader').contains('Авторизация прошла успешно');
     })
 
       it('Верный логин и неверный пароль', function () {
-        cy.get('#mail').type('german@dolnikov.ru');
-        cy.get('#pass').type('iLoveqastudio2');
+        cy.get('#mail').type('USER_LOGIN');
+        cy.get('#pass').type('неверный пароль');
         cy.get('#loginButton').click();
         cy.get('#messageHeader').should('be.visible');
         cy.get('#messageHeader').contains('Такого логина или пароля нет');
     })
       it('Валидация на наличие @', function () {
         cy.get('#mail').type('germandolnikov.ru');
-        cy.get('#pass').type('iLoveqastudio');
+        cy.get('#pass').type('USER_PASSWORD');
         cy.get('#loginButton').click();
         cy.get('#messageHeader').should('be.visible');
         cy.get('#messageHeader').contains('Нужно исправить проблему валидации');
@@ -34,7 +34,7 @@ describe('Проверка авторизации', function () {
 
        it('Восстановление пароля', function () {
         cy.get('#forgotEmailButton').click();
-        cy.get('#mailForgot').type('german@dolnikov.ru');
+        cy.get('#mailForgot').type('USER_LOGIN');
         cy.get('#restoreEmailButton').click();
         cy.get('#messageHeader').contains('Успешно отправили пароль на e-mail');
     })
