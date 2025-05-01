3770f8a88957fbd78d36293f10d6f9b2c0332a78
sellevan <sellevan0330@gmail.com>
2025-05-01 18:45:18 +0300

    Add files via upload

---
+++ cypress/cypress/e2e/buy_avatar.cy.js
@@ -0,0 +1,31 @@
+describe('Проверка покупки нового аватара', function () {
+
+    it('e2e тест на покупку нового аватара для тренера', function () {
+
+         cy.visit('https://pokemonbattle.ru/login');
+         cy.get('#k_email').type('sellevan0330@yandex.ru');
+         cy.get('#k_password').type('Evemin67');
+         cy.get('.MuiButton-root').click();
+         cy.wait(2000);
+         cy.get('.header_card_trainer').click();
+         cy.wait(1000);
+         cy.get('.k_mobile > :nth-child(5)').click();
+         cy.wait(1000);
+         cy.get('.available > button').first().click();
+         cy.get('.payment_form_card_form > :nth-child(2) > .style_1_base_input').type('4620869113632996');
+         cy.get(':nth-child(1) > .style_1_base_input').type('1226');  
+         cy.get('.payment_form_card_form_inputs > :nth-child(2) > .style_1_base_input').type('125');
+         cy.get('.payment_form_card_form_input_last > .style_1_base_input').type('NIKITA SALENKO');
+         cy.get('.style_1_base_button_payment_body > .style_1_base_button_payment').click();
+         cy.get('.style_1_base_input').type('56456');  
+         cy.get('.style_1_base_button_payment_body > .style_1_base_button_payment').click();
+         cy.contains('Покупка прошла успешно').should('be.visible');
+
+
+         //cy.get('.card_csv').type('125');                             // вводим CVV карты
+        // cy.get('.card_date').type('1226');                           // вводим срок действия карты
+        // cy.get('.card_name').type('NAME'); 
+
+
+    }) 
+})
---
+++ cypress/cypress/e2e/login.cy.js
@@ -0,0 +1,64 @@
+import * as data from "../helpers/default_data.json"
+import * as main_page from "../locators/main_page.json";
+import * as recovery_password_page from "../locators/recovery_password_page.json"
+import * as result_page from "../locators/result_page.json"
+
+describe('Проверка авторизации', function () {
+
+    beforeEach('Начало', function (){
+        cy.visit('/'); // Захожу на сайт
+          });
+
+          afterEach('Конец теста', function () {
+            cy.get(result_page.close).should('be.visible');
+           });
+   
+
+          it('Верный пароль и верный логин', function () {
+            cy.get(main_page.email).type(data.login);
+            cy.get(main_page.password).type(data.password);
+            cy.get(main_page.login_button).click();
+            cy.get(result_page.title).should('be.visible');
+            cy.get(result_page.title).contains('Авторизация прошла успешно');
+        })
+
+        it('проверка логики восстановления пароля', function () {
+            cy.get(main_page.fogot_pass_btn).should('have.css', 'color', 'rgb(0, 85, 152)');
+            cy.get(main_page.fogot_pass_btn).click();
+            cy.get(recovery_password_page.email).type('sellevan0330@yandex.ru');
+            cy.get(recovery_password_page.send_button).click();
+            cy.get(result_page.title).contains('Успешно отправили пароль на e-mail');
+        })
+
+        it('Верный логин и НЕверный пароль', function () {
+            cy.get(main_page.email).type(data.login);
+            cy.get('#pass').type('iLoveqastudio11');
+            cy.get(main_page.login_button).click();
+            cy.get(result_page.title).should('be.visible');
+            cy.get(result_page.title).contains('Такого логина или пароля нет');
+        })
+        
+        it('НЕверный логин и верный пароль', function () {
+            cy.get('#mail').type('sellevan@ya.ru');
+            cy.get(main_page.password).type(data.password);
+            cy.get(main_page.login_button).click();
+            cy.get(result_page.title).should('be.visible');
+            cy.get(result_page.title).contains('Такого логина или пароля нет');
+        })
+
+        it('Валидация на наличие @', function () {
+            cy.get(main_page.email).type('germandolnikov.ru');
+            cy.get(main_page.password).type('iLoveqastudio');
+            cy.get(main_page.login_button).click();
+            cy.get(result_page.title).should('be.visible');
+            cy.get(result_page.title).contains('Нужно исправить проблему валидации');
+        })
+
+        it('Проверка на приведение к строчным буквам в логине', function () {
+            cy.get('#mail').type('GerMan@Dolnikov.ru');
+            cy.get(main_page.password).type(data.password);
+            cy.get(main_page.login_button).click();
+            cy.get(result_page.title).should('be.visible');
+            cy.get(result_page.title).contains('Авторизация прошла успешно');
+        })
+})
